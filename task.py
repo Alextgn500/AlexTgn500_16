8894718fded789ef457f30d5079d0b5fdb3d6f20
Александр <azubkov500@gmail.com>
2025-01-22 20:26:54 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,23 +1,24 @@
-from fastapi import APIRouter
+# app/models/task.py
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+from app.backend.db import Base
+from app.models.user import User
 
-router = APIRouter(prefix="/task", tags=["task"])
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {"extend_existing": True}
 
-@router.get("/")
-async def all_tasks():
-    pass
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    slug = Column(String, unique=True, index=True)
 
-@router.get("/task_id")
-async def task_by_id():
-    pass
+    user = relationship('User', back_populates='tasks')
 
-@router.post("/create")
-async def create_task():
-    pass
 
-@router.put("/update")
-async def update_task():
-    pass
-
-@router.delete("/delete")
-async def delete_task():
-    pass
+# Для печати SQL-запроса
+print(CreateTable(Task.__table__))
